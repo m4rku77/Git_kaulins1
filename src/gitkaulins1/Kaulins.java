0cdd6741a60a7fa073bac846bade8866cba01e47
Jansons <markuss.jansons@sk.lvt.lv>
2024-04-16 11:57:00 +0300

    Iespēja mest kauliņu vairākas reizes

--- src/gitkaulins1/Kaulins.java
+++ src/gitkaulins1/Kaulins.java
@@ -3,18 +3,19 @@ package gitkaulins1;
 import java.util.Random;
 
 public class Kaulins {
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		int skaitlis;
 		
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.print("Tu uzmeti: "+skaitlis);
-
+		for(int i=0; i<reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Tu uzmeti: "+skaitlis);
+		}
 	}
 	
 	
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(5);
 		
 	
 	}
