b4c09c7bd72661e448ec8e19ffe26dfca426d909
Jansons <markuss.jansons@sk.lvt.lv>
2024-04-16 12:02:53 +0300

    vairākkārtīga kauliņu mešana bez metodes

--- src/gitkaulins1/Kaulins.java
+++ src/gitkaulins1/Kaulins.java
@@ -1,15 +1,21 @@
 package gitkaulins1;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
 
 	public static void main(String[] args) {
-		int skaitlis;
-		
+		int skaitlis, reizes;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.print("Tu uzmeti: "+skaitlis);
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mes kauliņi?");
+		reizes = scan.nextInt();
+		
+		for(int i=0; i<reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Tu uzmeti: "+skaitlis);
+		}
 	}
 
 }
