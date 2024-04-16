4081e3babcd9e79efbbc90e7c06abd3d7d60d04a
Jansons <markuss.jansons@sk.lvt.lv>
2024-04-16 11:44:04 +0300

    Ar kauliņu klasi

--- src/gitkaulins1/Kaulins.java
+++ src/gitkaulins1/Kaulins.java
@@ -1,10 +1,15 @@
 package gitkaulins1;
 
+import java.util.Random;
+
 public class Kaulins {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int skaitlis;
+		
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;
+		System.out.print("Tu uzmeti: "+skaitlis);
 	}
 
 }
